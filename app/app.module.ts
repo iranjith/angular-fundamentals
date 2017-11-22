4d46a8a1079a78b3f3c94b13226a4b34ce916f07
Ranjith kumar <ranjith.kumar3@in.ey.com>
2017-11-22 11:07:27 +0530

    Component Changes update2

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser'
 import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
-import {NavBarComponent} from './nav/navbar.component'
+import { NavBarComponent } from './nav/navbar.component'
 
 @NgModule({
   imports: [BrowserModule],
@@ -14,7 +14,6 @@ import {NavBarComponent} from './nav/navbar.component'
     EventThumbnailComponent,
     NavBarComponent
   ],
-    
   bootstrap: [EventsAppComponent]
 })
 export class AppModule {}
--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -6,13 +6,20 @@ import { Component, Input } from '@angular/core'
     <div class="well hoverwell thumbnail">
       <h2>{{event?.name}}</h2>
       <div>Date: {{event?.date}}</div>
-      <div>Time: {{event?.time}}</div>
+      <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
       <div>Price: \${{event?.price}}</div>
-      <div>
+      <div *ngIf="event?.location">
         <span>Location: {{event?.location?.address}}</span>
         <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
       </div>
-
+      <div *ngIf="event?.onlineUrl">
+        Online URL: {{event?.onlineUrl}}
+      </div>
     </div>
   `,
   styles: [`
@@ -24,4 +31,9 @@ import { Component, Input } from '@angular/core'
 export class EventThumbnailComponent {
   @Input() event:any
 
+  getStartTimeStyle() {
+    if (this.event && this.event.time === '8:00 am')
+      return {color: '#003300', 'font-weight': 'bold'}
+    return {}
+  }
 }
